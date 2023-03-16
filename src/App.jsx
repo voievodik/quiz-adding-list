645884791c9163ccb5d41b03ea70acb47a0791ae
voievodik <voevoda.taras@gmail.com>
2023-03-16 21:56:21 +0200

    added starting quiz, fixing some bags

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,29 @@ import './app.css';
 
 function App() {
   const [questions, setQuestions] = useState(data);
+  const [startQuiz, setStartQuiz] = useState(false);
+  const [number, setNumber] = useState(-1);
   
-  
+  const handleClick = () => {
+    setStartQuiz(!startQuiz);
+    !startQuiz && setNumber(0);
+  }  
 
   return (
     <div className="app">
-      {/* <StartQuestion questions={questions} /> */}
+      {number === questions.length || number === -1 ? 
+      <button 
+        className="button-start" 
+        onClick={handleClick}
+      >
+        {!startQuiz ? <span>Start Quiz</span> : <span>Edit Quiz</span>}
+      </button>
+      : <></>}
+      {startQuiz ?
+      <StartQuestion questions={questions} number={number} setNumber={setNumber} />
+      :
       <AddQuestion questions={questions} setQuestions={setQuestions} />
+      } 
     </div>
   )
 }
--- src/components/StartQuestion.jsx
+++ src/components/StartQuestion.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './startQuestion.css'
 
-export const StartQuestion = () => {
+export const StartQuestion = ({ questions, number, setNumber }) => {
+  
+  const [question, setQuestion] = useState(null);
+  const [mark, setMark] = useState(0)
+  const [correct, setCorrect] = useState(false)
+
+  useEffect(() => {
+    setQuestion(questions[number])
+  }, [number])
+  
+  const handleClick = (e, answer) => {
+    setCorrect(answer.correct)
+}
+
+const handleNext = () => {
+    setNumber(prev => prev + 1);
+    if(correct) setMark(prev => prev + 1)
+}
+
+const restartQuiz = () => {
+  setNumber(0);
+  setMark(0);
+}
+
   return (
-    <div>StartQuestion</div>
+    <div className="quiz-wrapper">
+      {question ?
+      <div className="quiz-question">
+        <h2 className='quiz-title'>{question?.question}</h2>
+        <ul className="quiz-answers">
+          <li onClick={(e) => handleClick(e, question.answers.answer1)}>{question?.answers.answer1.text}</li>
+          <li onClick={(e) => handleClick(e, question.answers.answer2)}>{question?.answers.answer2.text}</li>
+          <li onClick={(e) => handleClick(e, question.answers.answer3)}>{question?.answers.answer3.text}</li>
+          <li onClick={(e) => handleClick(e, question.answers.answer4)}>{question?.answers.answer4.text}</li>
+        </ul>
+        <button
+          className='quiz-button'
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+      :
+      <div className='correct-answers'>
+        <h2>Correct answers: {mark}</h2>
+        <div>
+          <button onClick={restartQuiz}>Restart Quiz</button>
+        </div>
+      </div>
+      }
+    </div>
   )
 }
